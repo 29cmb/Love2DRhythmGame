b9b303a1e0922b671c4ae4b0e8eaeb372eebd4f2
Cmb <71194682+29cmb@users.noreply.github.com>
2024-06-25 09:29:49 -0400

    Started On and On beats, could take a while

--- Songs/On and On/beats.lua
+++ Songs/On and On/beats.lua
@@ -1,6 +1,6 @@
 return {
     {
-       ["Time"] = 1.6,
+       ["Time"] = 0,
        ["Beats"] = {
             1,
             3
@@ -8,10 +8,72 @@ return {
        ["SpeedMod"] = 1
     },
     {
-        ["Time"] = 5.78,
+        ["Time"] = 3.48,
         ["Beats"] = {
             2,4
         },
         ["SpeedMod"] = 1
+    },
+    {
+        ["Time"] = 6,
+        ["Beats"] = {
+            1,3
+        },
+        ["SpeedMod"] = 1
+    },
+    {
+        ["Time"] = 6.2,
+        ["Beats"] = {
+            2,4
+        },
+        ["SpeedMod"] = 1
+    },
+    {
+        ["Time"] = 8.8,
+        ["Beats"] = {
+            1, 4
+        },
+        ["SpeedMod"] = 1
+    },
+    {
+        ["Time"] = 11.7,
+        ["Beats"] = {
+            2,3
+        },
+        ["SpeedMod"] = 1
+    },
+    {
+        ["Time"] = 15.8,
+        ["Beats"] = {
+            1
+        },
+        ["SpeedMod"] = 1
+    },
+    {
+        ["Time"] = 16,
+        ["Beats"] = {
+            2
+        },
+        ["SpeedMod"] = 1
+    },
+    {
+        ["Time"] = 16.2,
+        ["Beats"] = {
+            3
+        },
+        ["SpeedMod"] = 1
+    },
+    {
+        ["Time"] = 16.4,
+        ["Beats"] = {
+            4
+        },
+        ["SpeedMod"] = 1
+    },
+    {
+        ["Time"] = 20,
+        ["Beats"] = {
+            1,2
+        }
     }
-}
+} -- 2.3 subtraction from each beat from start to finish at a constant speed.
