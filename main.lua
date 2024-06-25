1cea31ff1a748adf93d1bfbc669cc90bb8129048
Cmb <71194682+29cmb@users.noreply.github.com>
2024-06-25 08:21:45 -0400

    Added one beat.

--- main.lua
+++ main.lua
@@ -1,7 +1,11 @@
 local self = {}
 local circleRadius = 20
 local spacing = (300 - (4 * circleRadius * 2)) / 5
-local circleY = 500 - circleRadius - 10
+local circleY = 500 - circleRadius - 40
+
+local Beats = require("beats")
+
+local speed = 3
 
 self.Colors = {
     [1] = {255/255, 0, 0},
@@ -17,6 +21,14 @@ self.KeyCodes = {
     [4] = "f"
 }
 
+self.Beats = {[1] = {
+    {
+        ["PosY"] = circleY - 90,
+        ["Hit"] = false
+    }
+}, [2] = {}, [3] = {}, [4] = {}}
+self.TimeSinceGameBegan = 0
+
 function love.load()
     love.window.setMode(300, 500)
 end
@@ -24,6 +36,17 @@ end
 function love.draw()
     for i = 1, 4 do
         local circleX = spacing * i + circleRadius * (2 * i - 1)
+
+        for _,beat in pairs(self.Beats[i]) do 
+            if beat.Hit == false then
+                love.graphics.setColor(self.Colors[i])
+                love.graphics.circle("fill", circleX, beat.PosY, circleRadius)
+                love.graphics.setColor(1,1,1)
+
+                beat.PosY = beat.PosY + speed
+            end
+        end
+
         if not love.keyboard.isDown(self.KeyCodes[i]) then
             love.graphics.circle("line", circleX, circleY, circleRadius)
         else
@@ -32,5 +55,15 @@ function love.draw()
             love.graphics.setColor(1, 1, 1)
         end
     end
+
+    -- for _, beat in pairs(beats) do
+    --     if math.floor(self.TimeSinceGameBegan) == beat.Time then
+
+    --     end
+    -- end
 end
 
+function love.update(dt)
+    Timer.update(dt)
+    self.TimeSinceGameBegan = self.TimeSinceGameBegan + dt
+end
