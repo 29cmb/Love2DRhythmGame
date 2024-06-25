608d539a199654df8aabd9f0cc6ce510a722aaa3
Cmb <71194682+29cmb@users.noreply.github.com>
2024-06-25 07:59:06 -0400

    Keys will light up colors

--- main.lua
+++ main.lua
@@ -3,22 +3,34 @@ local circleRadius = 20
 local spacing = (300 - (4 * circleRadius * 2)) / 5
 local circleY = 500 - circleRadius - 10
 
-self.colors = {
+self.Colors = {
     [1] = {255/255, 0, 0},
     [2] = {255/255, 150/255, 0},
     [3] = {255/255, 217/255, 0},
-    [4] = {128/255, 255/255, 0}
+    [4] = {5/255, 255/255, 0}
+}
+
+self.KeyCodes = {
+    [1] = "a",
+    [2] = "s",
+    [3] = "d",
+    [4] = "f"
 }
 
 function love.load()
     love.window.setMode(300, 500)
 end
 
-
 function love.draw()
     for i = 1, 4 do
         local circleX = spacing * i + circleRadius * (2 * i - 1)
-        love.graphics.circle("fill", circleX, circleY, circleRadius)
+        if not love.keyboard.isDown(self.KeyCodes[i]) then
+            love.graphics.circle("line", circleX, circleY, circleRadius)
+        else
+            love.graphics.setColor(self.Colors[i])
+            love.graphics.circle("fill", circleX, circleY, circleRadius)
+            love.graphics.setColor(1, 1, 1)
+        end
     end
 end
 
