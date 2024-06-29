be84dc512e94ee20e42c57da5e8473ee7c5f5b29
Cmb <71194682+29cmb@users.noreply.github.com>
2024-06-29 10:14:59 -0400

    Display all files

--- main.lua
+++ main.lua
@@ -182,8 +182,15 @@ function love.draw()
             love.filesystem.setIdentity("rhythm-game-levels")
             for _, v in ipairs(love.filesystem.getDirectoryItems("")) do
                 if v:match("^.+(%..+)$") == ".rhythm" then
-                    love.graphics.rectangle("fill", 2, (#levelPositions * 50) + 200, 400, 50)
+                    love.graphics.rectangle("fill", 2, (#levelPositions * 50) + 200, 400, 45)
+                    love.graphics.setColor(0,0,0)
+                    love.graphics.printf(v, 2, (#levelPositions * 50) + 200, 350)
+                    love.graphics.setColor(1,1,1)
                     print(10, (#levelPositions * 50), 400, 50)
+                    table.insert(levelPositions, {
+                        ["FileName"] = v,
+                        ["PosY"] = (#levelPositions * 50)
+                    })
                 end
             end
         end
